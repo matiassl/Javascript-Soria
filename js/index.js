192fbb0dfbf7bfaf3cfa29a3c2ef1bb2d1f2b36d
Matias SL <matiasdtc08@gmail.com>
2022-11-15 02:57:30 -0300

    E2

--- js/index.js
+++ js/index.js
@@ -1,89 +1,99 @@
+class producto {
+    constructor(id, nombre, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+let productos = [];
+
+let producto1 = new producto(1, "Hamburguesa Completa", 800);
+productos.push(producto1);
+
+let producto2 = new producto(2, "Piza Muzzarella", 1200);
+productos.push(producto2);
+
+let producto3 = new producto(3, "Empanadas", 1000);
+productos.push(producto3);
+
+let producto4 = new producto(4, "Ñoquis", 600);
+productos.push(producto4);
+
+
+
 function formaPago(total) {
     let formaPago = parseInt(prompt('Ingrese forma de pago \n 1.Debito \n 2.Credito'));
     if (formaPago === 1) {
         let tarjeta = parseInt(prompt("Ingrese numero de tarjeta"));
-        alert("Pago de " + total +" realizado con exito");
+        alert("Pago de " + total + " realizado con exito");
     }
     else if (formaPago === 2) {
-        let cuotas = parseInt(prompt('Ingrese el numero de cuotas \n 1 Cuota de' + total + ' \n 3 cuotas de ' + (total/3)*1.20 +   ' \n 6 cuotas de ' + (total/6)*1.40   ));
+        let cuotas = parseInt(prompt('Ingrese el numero de cuotas \n 1 Cuota de' + total + ' \n 3 cuotas de ' + (total / 3) * 1.20 + ' \n 6 cuotas de ' + (total / 6) * 1.40));
         let tarjeta = parseInt(prompt("Ingrese numero de tarjeta"));
-        if(cuotas === 1){
-            alert("Pago de " + total +" realizado con exito");
+        if (cuotas === 1) {
+            alert("Pago de " + total + " realizado con exito");
         }
-        else if(cuotas ===3){
-            alert("Pago de 3 cuotas de " + (total/3)*1.20 +" realizado con exito");
+        else if (cuotas === 3) {
+            alert("Pago de 3 cuotas de " + (total / 3) * 1.20 + " realizado con exito");
         }
 
-        else if(cuotas ===6){
-            alert("Pago de 6 cuotas de " + (total/6)*1.40 +" realizado con exito");
+        else if (cuotas === 6) {
+            alert("Pago de 6 cuotas de " + (total / 6) * 1.40 + " realizado con exito");
         }
     }
     return true;
 }
 
 
-function productos(total) {
-    let categoria = parseInt(prompt("Ingrese la categoria deseada: \n 1.Smartv \n 2.Audio \n 3.Playstation"));
-    let opcion;
-    if (categoria === 1) {
-        opcion = parseInt(prompt("Smartvs Disponibles: \n 1.Phillips 43 pulgadas $79.999 \n 2.Samsung 50 pulgadas $99.999 \n 3.Philco 32 $69.999"));
-        if (opcion === 1) {
-            //   carrito = carrito + 'Phillips 43 pulgadas $79.999 ';
-            total = total + 79999;
-        }
-        else if (opcion === 2) {
-           // carrito = carrito + 'Samsung 50 pulgadas $99.999 ';
-            total = total + 99999
-        }
-        else if (opcion === 3) {
-            // carrito = carrito + 'Philco 32 $69.999 ';
-            total = total + 69999;
-        }
+function mispedidos(total) {
+    let pedido;
+    let menu = `Elija una opcion de nuestro menu del dia: `;
+    for (item of productos) {
+        menu += `\n  ${item.id}. ${item.nombre} $ ${item.precio} `;
     }
-    else if (categoria === 2) {
-        opcion = parseInt(prompt("Productos Disponibles: \n 1.Auriculares Sony $5999 \n 2.Auriculares Sony Inalambricos $8999 \n 3.Parlante Inalambrico Sony $129.999"));
-        if (opcion === 1) {
-            //  carrito = carrito + 'Auriculares Sony $5999 ';
-            total = total + 5999;
-        }
-        else if (opcion === 2) {
-            // carrito = carrito + 'Auriculares Sony Inalambricos $8999 ';
-            total = total + 89999;
-        }
-        else if (opccion === 3) {
-            // carrito = carrito + 'Parlante Inalambrico Sony $129.999 ';
-            total = total + 129999;
-        }
+   
+    let opcion = parseInt(prompt(`${menu}`));
+
+
+    while (opcion < 1 || opcion > productos.length || isNaN(opcion)) {
+        opcion = parseInt(prompt(`${menu}`));
     }
-    else if (categoria === 3) {
-        opcion = parseInt(prompt("Playstations disponibles: \n 1.Playstation 5 $329000 \n 2.Playstation 4 $150000"));
-        if (opcion === 1) {
-           // carrito = carrito + 'Playstation 5 $329000 ';
-            total = total + 329000;
-        }
-        else if (opcion === 2) {
-            //carrito = carrito + 'Playstation 4 $150000 ';
-            total = total + 150000;
-        }
+    pedido = productos.find(e => e.id === opcion);
+    if (pedido !== undefined) {
+        carrito.push(pedido);
     }
 
-    return total;
+    console.log(carrito);
+}
+
+function mostrarCompra() {
+    let lista ="";
+    let precio = 0;
+
+    for (item of carrito) {
+        lista += `${item.nombre} - `;
+        
+        precio = item.precio +precio;
+  
+    }
+    alert(`Productos deseados: ${lista} \n Precio total ${precio}`)
+
+    return precio
 }
 
 
-
-
 let comprar = true;
-let carrito;
+let carrito = [];
 let precioTotal = 0;
 let nombre = prompt('Ingrese su nombre');
 let preguntar;
-alert(`Hola ${nombre}, Bienvenido a nuestra tienda online`);
+alert(`Hola ${nombre}, Bienvenido a nuestra tienda de Comida`);
 
 
 while (comprar === true) {
-    precioTotal = productos(precioTotal);
-    alert('Total ' + precioTotal);
+    mispedidos();
+
+    precioTotal = mostrarCompra(carrito);
 
     preguntar = parseInt(prompt("Desea seguir comprando? 1.Si 2.No"));
     if (preguntar === 2) {
@@ -93,6 +103,6 @@ while (comprar === true) {
 
 }
 
-if(formaPago(precioTotal)){
+if (formaPago(precioTotal)) {
     alert('Gracia por su compra, vuelvas prontos')
 }
