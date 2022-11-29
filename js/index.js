82f6dedd3bce5de87015e8fdb109538b2b11710e
Matias SL <matiasdtc08@gmail.com>
2022-11-29 01:30:51 -0300

    Entrega3

--- js/index.js
+++ js/index.js
@@ -20,89 +20,119 @@ let producto4 = new producto(4, "Ñoquis", 600);
 productos.push(producto4);
 
 
+const divProductos = document.getElementById('productos');
 
-function formaPago(total) {
-    let formaPago = parseInt(prompt('Ingrese forma de pago \n 1.Debito \n 2.Credito'));
-    if (formaPago === 1) {
-        let tarjeta = parseInt(prompt("Ingrese numero de tarjeta"));
-        alert("Pago de " + total + " realizado con exito");
-    }
-    else if (formaPago === 2) {
-        let cuotas = parseInt(prompt('Ingrese el numero de cuotas \n 1 Cuota de' + total + ' \n 3 cuotas de ' + (total / 3) * 1.20 + ' \n 6 cuotas de ' + (total / 6) * 1.40));
-        let tarjeta = parseInt(prompt("Ingrese numero de tarjeta"));
-        if (cuotas === 1) {
-            alert("Pago de " + total + " realizado con exito");
-        }
-        else if (cuotas === 3) {
-            alert("Pago de 3 cuotas de " + (total / 3) * 1.20 + " realizado con exito");
-        }
+productos.forEach(prod => {
+    divProductos.innerHTML += `
+    <div class="col-sm-4">
+        <div class="card" style="width: 100%;" id="${prod.id}">
+            
+                <div class="card-body">
+                    <h5 class="card-title">${prod.nombre}</h5>
+                    <p class="card-text">${prod.precio}</p>
+                    <button class="btn btn-primary" id="${prod.id}">Agregar al carrito</button>
+                </div>
+        </div>
+    </div>`
+});
 
-        else if (cuotas === 6) {
-            alert("Pago de 6 cuotas de " + (total / 6) * 1.40 + " realizado con exito");
-        }
+const carrito = [];
+const botonesAgregar = document.querySelectorAll('.btn-primary');
+
+
+botonesAgregar.forEach(boton=>{
+    boton.onclick = () => {
+        const prodselecionado = productos.find(prod=>prod.id === boton.id)
+        console.log(prodselecionado)
     }
-    return true;
 }
+)
 
 
-function mispedidos(total) {
-    let pedido;
-    let menu = `Elija una opcion de nuestro menu del dia: `;
-    for (item of productos) {
-        menu += `\n  ${item.id}. ${item.nombre} $ ${item.precio} `;
-    }
+
+
+// function formaPago(total) {
+//     let formaPago = parseInt(prompt('Ingrese forma de pago \n 1.Debito \n 2.Credito'));
+//     if (formaPago === 1) {
+//         let tarjeta = parseInt(prompt("Ingrese numero de tarjeta"));
+//         alert("Pago de " + total + " realizado con exito");
+//     }
+//     else if (formaPago === 2) {
+//         let cuotas = parseInt(prompt('Ingrese el numero de cuotas \n 1 Cuota de' + total + ' \n 3 cuotas de ' + (total / 3) * 1.20 + ' \n 6 cuotas de ' + (total / 6) * 1.40));
+//         let tarjeta = parseInt(prompt("Ingrese numero de tarjeta"));
+//         if (cuotas === 1) {
+//             alert("Pago de " + total + " realizado con exito");
+//         }
+//         else if (cuotas === 3) {
+//             alert("Pago de 3 cuotas de " + (total / 3) * 1.20 + " realizado con exito");
+//         }
+
+//         else if (cuotas === 6) {
+//             alert("Pago de 6 cuotas de " + (total / 6) * 1.40 + " realizado con exito");
+//         }
+//     }
+//     return true;
+// }
+
+
+// function mispedidos(total) {
+//     let pedido;
+//     let menu = `Elija una opcion de nuestro menu del dia: `;
+//     for (item of productos) {
+//         menu += `\n  ${item.id}. ${item.nombre} $ ${item.precio} `;
+//     }
    
-    let opcion = parseInt(prompt(`${menu}`));
+//     let opcion = parseInt(prompt(`${menu}`));
 
 
-    while (opcion < 1 || opcion > productos.length || isNaN(opcion)) {
-        opcion = parseInt(prompt(`${menu}`));
-    }
-    pedido = productos.find(e => e.id === opcion);
-    if (pedido !== undefined) {
-        carrito.push(pedido);
-    }
+//     while (opcion < 1 || opcion > productos.length || isNaN(opcion)) {
+//         opcion = parseInt(prompt(`${menu}`));
+//     }
+//     pedido = productos.find(e => e.id === opcion);
+//     if (pedido !== undefined) {
+//         carrito.push(pedido);
+//     }
 
-    console.log(carrito);
-}
+//     console.log(carrito);
+// }
 
-function mostrarCompra() {
-    let lista ="";
-    let precio = 0;
+// function mostrarCompra() {
+//     let lista ="";
+//     let precio = 0;
 
-    for (item of carrito) {
-        lista += `${item.nombre} - `;
+//     for (item of carrito) {
+//         lista += `${item.nombre} - `;
         
-        precio = item.precio +precio;
+//         precio = item.precio +precio;
   
-    }
-    alert(`Productos deseados: ${lista} \n Precio total ${precio}`)
+//     }
+//     alert(`Productos deseados: ${lista} \n Precio total ${precio}`)
 
-    return precio
-}
-
-
-let comprar = true;
-let carrito = [];
-let precioTotal = 0;
-let nombre = prompt('Ingrese su nombre');
-let preguntar;
-alert(`Hola ${nombre}, Bienvenido a nuestra tienda de Comida`);
+//     return precio
+// }
 
 
-while (comprar === true) {
-    mispedidos();
-
-    precioTotal = mostrarCompra(carrito);
-
-    preguntar = parseInt(prompt("Desea seguir comprando? 1.Si 2.No"));
-    if (preguntar === 2) {
-        comprar = false;
-    }
+// let comprar = true;
+// let carrito = [];
+// let precioTotal = 0;
+// let nombre = prompt('Ingrese su nombre');
+// let preguntar;
+// alert(`Hola ${nombre}, Bienvenido a nuestra tienda de Comida`);
 
 
-}
+// while (comprar === true) {
+//     mispedidos();
 
-if (formaPago(precioTotal)) {
-    alert('Gracia por su compra, vuelvas prontos')
-}
+//     precioTotal = mostrarCompra(carrito);
+
+//     preguntar = parseInt(prompt("Desea seguir comprando? 1.Si 2.No"));
+//     if (preguntar === 2) {
+//         comprar = false;
+//     }
+
+
+// }
+
+// if (formaPago(precioTotal)) {
+//     alert('Gracia por su compra, vuelvas prontos')
+// }
