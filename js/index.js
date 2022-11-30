355bf6d2e28a20400d6c3d8b5642257185ec4200
Matias SL <matiasdtc08@gmail.com>
2022-11-30 06:53:50 -0300

    t3

--- js/index.js
+++ js/index.js
@@ -5,18 +5,18 @@ class producto {
         this.precio = precio;
     }
 }
-let productos = [];
+const productos = [];
 
-let producto1 = new producto(1, "Hamburguesa Completa", 800);
+const producto1 = new producto(1, "Hamburguesa Completa", 800);
 productos.push(producto1);
 
-let producto2 = new producto(2, "Piza Muzzarella", 1200);
+const producto2 = new producto(2, "Piza Muzzarella", 1200);
 productos.push(producto2);
 
-let producto3 = new producto(3, "Empanadas", 1000);
+const producto3 = new producto(3, "Empanadas", 1000);
 productos.push(producto3);
 
-let producto4 = new producto(4, "Ñoquis", 600);
+const producto4 = new producto(4, "Ñoquis", 600);
 productos.push(producto4);
 
 
@@ -25,7 +25,7 @@ const divProductos = document.getElementById('productos');
 productos.forEach(prod => {
     divProductos.innerHTML += `
     <div class="col-sm-4">
-        <div class="card" style="width: 100%;" id="${prod.id}">
+        <div class="card" style="width: 100%;" id="div${prod.id}">
             
                 <div class="card-body">
                     <h5 class="card-title">${prod.nombre}</h5>
@@ -38,16 +38,40 @@ productos.forEach(prod => {
 
 const carrito = [];
 const botonesAgregar = document.querySelectorAll('.btn-primary');
-
+console.log(botonesAgregar)
 
 botonesAgregar.forEach(boton=>{
     boton.onclick = () => {
-        const prodselecionado = productos.find(prod=>prod.id === boton.id)
-        console.log(prodselecionado)
+        const prodselecionado = productos.find(prod=>prod.id === parseInt(boton.id))
+        // console.log(prodselecionado)
+        // console.log(boton.id)
+
+        const productoCarrito = {...prodselecionado, cantidad:1}
+
+        const indexCarrito = carrito.findIndex(prod=> prod.id === productoCarrito.id)
+
+        if(indexCarrito === -1){
+            carrito.push(productoCarrito)
+        }
+        else{
+            carrito[indexCarrito].cantidad ++
+        }
+        console.log(carrito)
+
     }
 }
 )
 
+const finalizar = document.querySelector('#finalizar')
+finalizar.onclick = () => {
+    const valores = carrito.map(prod => prod.precio * prod.cantidad) 
+    let totalCompra =0
+    valores.forEach(valor =>{
+        totalCompra += valor
+    })
+    console.log(valores)
+    console.log(totalCompra)
+}
 
 
 
